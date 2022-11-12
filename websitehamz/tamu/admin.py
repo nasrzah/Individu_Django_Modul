238820b479b7378f8f9fac410a33b779cc3583db
Hamzah Nasrulloh <nasrullohhamzah@gmail.com>
2022-11-12 15:46:07 +0700

    Tugas Individu

---
+++ websitehamz/tamu/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from . models import Guest
+
+class TampilGuest(admin.ModelAdmin):
+    list_display = ('nim', 'nama' , 'kegiatan')
+    list_display_links: (None)
+    search_fields = ('nim' , 'nama')
+
+admin.site.register(Guest)
